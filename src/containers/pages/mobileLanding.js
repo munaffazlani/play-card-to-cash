54a4388b64509d658ecf22dec883bcd0e2e1437d
Munaf Fazlani <munaffazlani@gmail.com>
2020-10-05 01:35:00 +0530

    setting up still

---
+++ src/containers/pages/mobileLanding.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Flipper from "./components/flipper";
+import StepsCard from "./components/stepsCard";
+import Drawer from "./components/drawer";
+import Header from "./components/header";
+import Login from "@app/components/login";
+import Banner from "./components/banner";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Banner />
+      <Drawer>
+        <Login />
+      </Drawer>
+      <StepsCard />
+      <StepsCard />
+      <StepsCard />
+      <StepsCard />
+    </div>
+  );
+}
+
+export default App;
